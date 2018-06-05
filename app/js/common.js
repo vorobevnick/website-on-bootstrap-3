bdb36a8c51717779abc6599e730b28aaac0b59ef
vorobev.nick <sparinf@mail.ru>
2018-06-05 19:20:55 +0500

    section products

--- app/js/common.js
+++ app/js/common.js
@@ -32,4 +32,36 @@ $(function() {
         ]
     });
 
+    $('.slider-reviews').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        centerMode: true,
+        centerPadding: '0px',
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+
 });
